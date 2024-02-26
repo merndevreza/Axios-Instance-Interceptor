28783ce7ff9472fa125588d6b18da5638425d881
merndevreza <merndevreza@gmail.com>
2024-02-26 11:35:10 +0600

    Add, Edit, Delete -  all features works without axios

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,47 @@ import AddEditTasks from "./components/AddEditTasks";
 
 function App() {
   const [tasks, setTasks] = useState(data);
-  const [editTask,setEditTask]=useState(null); 
-  const handleAddTask=(newTask)=>{
-    const nexId=tasks.length?tasks[tasks.length-1].id +1:1;
+  const [editTask, setEditTask] = useState(null);
+
+  const handleAddTask = (newTask) => {
+    const nexId = tasks.length ? tasks[tasks.length - 1].id + 1 : 1;
     setTasks([
       ...tasks,
       {
         ...newTask,
-        id:nexId
+        id: nexId,
+      },
+    ]);
+  };
+  const handleDeleteTask = (id) => {
+    const newTasks = tasks.filter((task) => task.id !== id);
+    setTasks(newTasks);
+  };
+  const handleEditTask = (task) => {
+    const UpdatedTasks = tasks.map((t) => {
+      if (t.id === task.id) {
+        return task;
+      } else {
+        return t;
       }
-    ]) 
-  }
-  const handleDeleteTask=(id)=>{
-    const newTasks=tasks.filter(task=>task.id!==id);
-    setTasks(newTasks)
-  }
-  const handleEditTask=(task)=>{
-const find=tasks.find(t=>t.id===task.id)
-console.log(find);
-  }
-  
+    });
+    setTasks(UpdatedTasks);
+    setEditTask(null)
+  };
+
   return (
     <div className="bg-pink-100 p-5">
       <h1 className="text-2xl  border-b-2 border-black">Axios</h1>
-      <Tasks tasks={tasks} onDelete={handleDeleteTask} setEditTask={setEditTask}/>
-      <AddEditTasks editTask={editTask}  onAdd={handleAddTask} onEdit={handleEditTask}/>
+      <Tasks
+        tasks={tasks}
+        onDelete={handleDeleteTask}
+        setEditTask={setEditTask}
+      />
+      <AddEditTasks
+        editTask={editTask}
+        onAdd={handleAddTask}
+        onEdit={handleEditTask}
+      />
     </div>
   );
 }
